b97dfa83a7309a1ac1f660323792b7f28c69678a
Hoppifol <167127450+Hoppifol@users.noreply.github.com>
2025-03-19 22:00:59 +0300

    Add files via upload

---
+++ ClassLibrary1/ClassLibrary1/Class1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace ClassLibrary1
+{
+    public class Class1
+    {
+        public static bool ValidateEmail(string email)
+        {
+            if (string.IsNullOrEmpty(email))
+                return false;
+
+            string pattern = @"^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$";
+            return Regex.IsMatch(email, pattern);
+        }
+
+        public static bool ValidatePhone(string phone)
+        {
+            if (string.IsNullOrEmpty(phone))
+                return false;
+
+            string pattern = @"^\+7\d{10}$";
+            return Regex.IsMatch(phone, pattern);
+        }
+
+        public static bool ValidatePassword(string password)
+        {
+            if (string.IsNullOrEmpty(password))
+                return false;
+
+            string pattern = @"^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$";
+            return Regex.IsMatch(password, pattern);
+        }
+    }
+}
